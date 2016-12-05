45c9802e6527dfb908f58ce51dca2a90da7edb07
GuillelmoGomezCeballos <ceballos@cern.ch>
2016-12-05 14:37:45 +0100

    new

--- bin/80x/mit/firstSkimming_da.sh
+++ bin/80x/mit/firstSkimming_da.sh
@@ -3,18 +3,18 @@
 export theRND=file_skim_$RANDOM;
 
 export INPUTDIR=/mnt/hadoop/cms/store/user/dhsu/Nero/v1.3/$1;
-export SKIMDIR=/data/t3home000/ceballos/Nero/skim_80x;
+export SKIMDIR=/data/t3home000/ceballos/Nero/skim_80x/$1;
 
 find $INPUTDIR/* -name '000?'| awk '{split($1,a,ENVIRON["INPUTDIR"]);print a[2]}' > $theRND.txt
 
 for datasetName in `cat $theRND.txt`; do
   echo "Merging dataset: "$INPUTDIR/$datasetName
-  ls $INPUTDIR/$datasetName|grep root > $INPUTDIR/$datasetName.txt;
   mkdir -p $SKIMDIR/$datasetName;
-  for fileName in `cat $INPUTDIR/$datasetName.txt`; do
+  ls $INPUTDIR/$datasetName|grep root > $SKIMDIR/$datasetName.txt;
+  for fileName in `cat $SKIMDIR/$datasetName.txt`; do
     root -l -q -b MitAnalysisRunII/skimming/80x/makeOneSkimSample.C+\(\"$INPUTDIR/$datasetName/$fileName\",\"$SKIMDIR/$datasetName/$fileName\",\"data\",0,0,0\);
   done
-  rm -f $INPUTDIR/$datasetName.txt;
+  rm -f $SKIMDIR/$datasetName.txt;
 done
 
 rm -f $theRND.txt;
